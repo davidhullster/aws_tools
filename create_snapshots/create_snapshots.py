151f5dd9058d71cbaf2dc25cc8376eee85134fc2
dhull <david_hull@intuit.com>
2018-04-29 22:51:38 -0700

    update to add click decorators

--- create_snapshots/create_snapshots.py
+++ create_snapshots/create_snapshots.py
@@ -5,18 +5,27 @@ session = boto3.Session(profile_name='guest')
 ec2 = session.resource('ec2')
 
 @click.command()
-def list_instances():
-	"List EC2 instances"
-	for i in ec2.instances.all():
-	    print(', '.join((
-	    	i.id,
-	    	i.instance_type,
-	    	i.placement['AvailabilityZone'],
-	    	i.state['Name'],
-	    	i.public_dns_name,
-	    	i.public_ip_address)))
+@click.option('--name', default=None,
+    help="Only instances for name (tag Name:<name>)")
+def list_instances(name):
+    "List EC2 instances"
+    instances = []
 
-	return
+    if name:
+        filters = [{'Name':'tag:Name', 'Values':[name]}]
+        instances = ec2.instances.filter(Filters=filters)
+    else:
+        instances = ec2.instances.all()
+    for i in instances:
+        print(', '.join((
+           i.id,
+           i.instance_type,
+           i.placement['AvailabilityZone'],
+           i.state['Name'],
+           i.public_dns_name
+        )))
+
+    return
 
 if __name__ == '__main__':
-	list_instances()
+    list_instances()
