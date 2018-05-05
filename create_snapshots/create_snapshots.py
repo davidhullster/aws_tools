fb9c5daeab8364e0a02b5e58795dc37aca9a72ab
dhull <david_hull@intuit.com>
2018-05-05 12:10:17 -0700

    handle error from aws when starting or stopping instances when in pending state

--- create_snapshots/create_snapshots.py
+++ create_snapshots/create_snapshots.py
@@ -1,5 +1,6 @@
 import boto3
 import click
+import botocore
 
 session = boto3.Session(profile_name='guest')
 ec2 = session.resource('ec2')
@@ -136,7 +137,11 @@ def stop_instances(name):
 
     for i in instances:
         print("Stopping {0}...".format(i.id))
-        i.stop()
+        try:
+            i.stop()
+        except botocore.exceptions.ClientError as e:
+            print(" Could not stop{0}. ".format(i.d) + str(e))
+            continue
 
     return
 
@@ -150,7 +155,11 @@ def start_instances(name):
 
     for i in instances:
         print("Starting {0}...".format(i.id))
-        i.start()
+        try:
+            i.start()
+        except botocore.exceptions.ClientError as e:
+            print(" Could not start{0}. ".format(i.id) + str(e))
+            continue
 
     return
 
